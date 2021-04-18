45663287f61e00878ea8cd7934cc7260ecd892da
Riller <rvsriller@gmail.com>
2021-04-18 01:56:55 -0300

    Day 2: Operations

--- 30daysOfjavascript/day2.js
+++ 30daysOfjavascript/day2.js
@@ -1,5 +1,6 @@
 /**
     Day 2 - Operations
+    
     Task
     Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), 
     and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. 
