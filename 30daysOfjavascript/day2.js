30c92db6ce273c2655fbd00171679df0ad558c79
Riller <rvsriller@gmail.com>
2021-04-17 22:31:54 -0300

    Day 2: Operations

--- 30daysOfjavascript/day2.js
+++ 30daysOfjavascript/day2.js
@@ -1,5 +1,6 @@
 /**
- *  Task
+    Day 2 - Operations
+    Task
     Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), 
     and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. 
     Round the result to the nearest integer.
