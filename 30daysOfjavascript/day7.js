acb2c3fa8371484a69b5267246b45fa84eeb7a6c
Riller <rvsriller@gmail.com>
2021-04-20 23:14:07 -0300

    Day 7: Array

--- 30daysOfjavascript/day7.js
+++ 30daysOfjavascript/day7.js
@@ -2,6 +2,7 @@
     Day 7 - Array 
     
     Today, we will learn about the Array data structure
+    
     Task
     Given an array, A, of N integers, print A's elements in reverse order
     as a single line of space-separated numbers.
