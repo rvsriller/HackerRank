832231143ff0b91ca382639ee589fbaddb9bbe29
Riller <rvsriller@gmail.com>
2021-04-27 22:37:02 -0300

    Day 8: Map and Dictionary data structure

--- 30daysOfjavascript/day8.js
+++ 30daysOfjavascript/day8.js
@@ -1,9 +1,11 @@
 /**
- * Today, we're learning about Key-Value pair mappings using a Map or Dictionary data structure
-Task
-Given n names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. 
-You will then be given an unknown number of names to query your phone book for. For each NAME queried, print the associated
-entry from your phone book on a new line in the form name=phoneNumber; if an entry for NAME  is not found, print Not found instead. 
+    Today, we're learning about Key-Value pair mappings using a Map or Dictionary data structure
+
+    Task
+    Given n names and phone numbers, assemble a phone book that maps friends' names to their respective phone numbers. 
+    You will then be given an unknown number of names to query your phone book for. For each NAME queried, print the associated
+    entry from your phone book on a new line in the form name=phoneNumber; if an entry for NAME  is not found, print Not found instead. 
+
 */
 
 
