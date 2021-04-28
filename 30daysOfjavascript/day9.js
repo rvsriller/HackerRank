9834e9c5bf17f14688c27bed1a9cd245e4e568f2
Riller <rvsriller@gmail.com>
2021-04-28 06:41:58 -0300

    Day 9: Recursion

---
+++ 30daysOfjavascript/day9.js
@@ -0,0 +1,33 @@
+/*
+Algorithmic concept called recursion using factorial(n)
+    
+    factorial(n) = n * (factorial(n-1));
+    5! = 5 x 4 x 3 x 2 x 1 
+
+*/
+
+// Complete the factorial function below.
+function factorial(n) {
+    
+    if(n == 0 || n == 1){
+        return 1;
+    }else{
+        return (n * factorial(n-1));;
+    }
+    
+}
+
+
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let result = factorial(n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
