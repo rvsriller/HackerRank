66b473f7e8ad49400de82c37df9e05bd8883f2c4
Riller <rvsriller@gmail.com>
2021-04-29 23:37:48 -0300

    Day 10: Binary

--- 30daysOfjavascript/day10.js
+++ 30daysOfjavascript/day10.js
@@ -1,3 +1,30 @@
+/*  Longest solution
+    const n = parseInt(readLine(), 10);    
+    let stack = [], reminder = '', counter = 0;
+    
+    //Transform number to binary
+    while(n > 0){
+        reminder = n % 2;
+        n = Math.floor(n / 2);
+        
+        //Insert reminder to stack ex: 01101 push(1) -> 011011
+        stack.push(reminder);
+    }
+    
+    //Using sort to compare two elements in row, if they are equal to one, there is a sequence!
+    stack.sort((a, b) => {
+        if(reminder == 1 && counter == 0){
+            counter = 1;
+        }else if(a == 1 && b == 1) {
+            counter++;
+        }
+        
+    })
+    
+    //Showing how many 11's exists 
+    console.log(counter);
+*/
+
 
     
     //calculate maximum number of consecutive 1's in the binary representation of (n)
