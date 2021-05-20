a0f1837070ea698712b02849c76382560ab52053
Riller <rvsriller@gmail.com>
2021-05-19 22:40:20 -0300

    Day 10: Binary

--- 30daysOfjavascript/day10.js
+++ 30daysOfjavascript/day10.js
@@ -48,7 +48,7 @@
   
     */    
     
-    //calculate maximum number of consecutive 1's in the binary representation of (n)
+    //it calculates the maximum number of consecutive 1's in the binary representation of (n)
     let result =  Math.max.apply(null, 
                     n.toString(2). //Converts it to binary
                     split("0"). // Creates an array without "0"s
