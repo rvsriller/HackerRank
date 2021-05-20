6549b1cc5a95ddc46e5b4722c425e283ee2dcdf3
Riller <rvsriller@gmail.com>
2021-05-20 20:20:48 -0300

    Day 4: Class and Instance

--- 30daysOfjavascript/day4.js
+++ 30daysOfjavascript/day4.js
@@ -56,7 +56,7 @@ for(i = 0; i < T; i++){
     var age = parseInt(readLine());
     var p=new Person(age);
     p.amIOld();
-    for(j = 0;j<3;j++){
+    for(j = 0; j < 3; j++){
         p.yearPasses();
         
     }
